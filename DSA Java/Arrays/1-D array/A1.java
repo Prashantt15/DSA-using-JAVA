7eee9ab30d1682feb08068028cbb84f5e84c5c70
Prashant sharma <15prashant.sr@gmail.com>
2025-01-09 11:30:27 +0530

    Update A1.java

--- DSA Java/Arrays/1-D array/A1.java
+++ DSA Java/Arrays/1-D array/A1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class A1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter how many elements present in array ");
+        System.out.println("Enter elements in array ");
         int n=sc.nextInt();
         int[] arr=new int[n];
         System.out.println("Enter elements value");
